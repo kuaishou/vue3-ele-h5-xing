1dd5cb9667fc38da5c57b81a432afe1ecbe752a6
hb_dasheng <947634673@qq.com>
2024-06-13 19:07:37 +0800

    feat: 配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:8000/',
+      '/imgs': 'http://localhost:8000/'
     }
   }
 })
